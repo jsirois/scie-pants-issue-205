f966bb0a0c986e7e1ade31101c4cceed51d2d93a
John Sirois <john.sirois@gmail.com>
2023-06-06 18:47:25 -0700

    Add the steps used.

--- run.sh
+++ run.sh
@@ -8,5 +8,6 @@ log "Launching services..."
 
 docker-compose up -d
 
-log 'When complete, run `fly -t tutorial login -c http://localhost:8080 -u test -p test`'
-
+log 'When complete, run `fly -t tutorial login -c http://localhost:8080 -u test -p test`.'
+log 'Then set the pipeline with `fly -t tutorial set-pipeline -p build -c example.yml`.'
+log 'You can then go to the UI (maybe unpause) and start a job.'
